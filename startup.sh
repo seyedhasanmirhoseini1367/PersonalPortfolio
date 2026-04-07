2ee6e7081af4c046bbdca3ee2b60a143a61909c0
seyedhasanmirhoseini1367 <seyedhasan.mirhoseini1367@gmail.com>
2026-04-07 23:43:20 +0300

    Add Azure deploy step to CI and startup.sh

---
+++ startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Azure App Service startup script
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+gunicorn PersonalPortfolio.wsgi:application \
+    --bind 0.0.0.0:8000 \
+    --workers 2 \
+    --timeout 120 \
+    --access-logfile - \
+    --error-logfile -
