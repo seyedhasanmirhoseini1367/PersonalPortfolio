7b53f1bccebd1cf0e623bcaf0944e7291a28bc5e
seyedhasanmirhoseini1367 <seyedhasan.mirhoseini1367@gmail.com>
2026-04-09 16:28:25 +0300

    Auto-create superuser on startup

--- startup.sh
+++ startup.sh
@@ -2,6 +2,17 @@
 # Azure App Service startup script
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+
+# Create superuser if it doesn't exist yet
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='seyed').exists():
+    User.objects.create_superuser('seyed', 'seyed@example.com', 'Seyed@2024')
+    print('Superuser created.')
+else:
+    print('Superuser already exists.')
+"
 gunicorn PersonalPortfolio.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 2 \
