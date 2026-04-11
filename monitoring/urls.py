d5ec1c77ff973a988b8fddc794617d78968eff13
seyedhasanmirhoseini1367 <seyedhasan.mirhoseini1367@gmail.com>
2026-04-11 11:43:16 +0300

    fix: add storage debug view

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 app_name = 'monitoring'
 
+
 urlpatterns = [
     path('',           views.dashboard,  name='dashboard'),
     path('api/stats/', views.api_stats,  name='api_stats'),
     path('storage/',   views.storage_debug, name='storage_debug'),
+    path('debug/storage/', views.storage_debug),
 ]
